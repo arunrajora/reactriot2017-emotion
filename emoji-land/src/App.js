888ac63fe45cf80ea69d011c493a4ad579a51a25
arunrajora <rajora.rajora2@outlook.com>
2017-06-25 03:30:05 +0530

    create empty components.

--- emoji-land/src/App.js
+++ emoji-land/src/App.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import firebase from 'firebase';  
+import reactfire from 'reactfire';
+
+const config = {
+    apiKey: "AIzaSyDksGHSF4G1Bf9OaTV4_6uknskxAZh_k8Q",
+    authDomain: "emoji-land-4b7d8.firebaseapp.com",
+    databaseURL: "https://emoji-land-4b7d8.firebaseio.com",
+    projectId: "emoji-land-4b7d8",
+    storageBucket: "emoji-land-4b7d8.appspot.com",
+    messagingSenderId: "851877396193"
+  };
+firebase.initializeApp(config);
+
 
 class App extends Component {
+
+    componentWillMount(){
+        this.firebaseRef=firebase.database().ref("heha");
+    }
+
+    componentWillUnmount(){
+        this.firebaseRef.off();
+    }
     render() {
         return (
             <div className="app">
